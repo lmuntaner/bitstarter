3ed011300e2163df89d0ccc4e611ad77e2b2f018
Llorenc Muntaner <llorenc.muntaner@gmail.com>
2013-07-09 11:23:49 +0000

    Updated web.js with index.html again12

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8')
+var str = 'Hello World3';
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
