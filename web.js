e78e5eeb9bfa6565f572147c156b237fbe2eccdf
Llorenc Muntaner <llorenc.muntaner@gmail.com>
2013-07-09 10:56:25 +0000

    Updated web.js with index.html again3

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var fs = require('fs');
 
 var homepage = fs.readFileSync('index.html');
 var buf = new Buffer(homepage);                                              
+var file_text = buf.toString('utf8');
 
 App.get('/', function(request, response) {
-  response.send(homepage.toString('utf8'));
+  response.send(file_text);
 });
 
 var port = process.env.PORT || 5000;
