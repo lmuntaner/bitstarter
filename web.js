12ec68efd6b13238f02125bddab7d7d501ff6d98
Llorenc Muntaner <llorenc.muntaner@gmail.com>
2013-07-09 11:25:18 +0000

    Updated web.js with index.html again13

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8')
-var str = 'Hello World3';
+var str = buf.toString();
 
 app.get('/', function(request, response) {
   response.send(str);
