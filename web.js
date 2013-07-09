bf513f7434e6df16f8d1fa59dc14ac245fbbfd64
Llorenc Muntaner <llorenc.muntaner@gmail.com>
2013-07-09 11:20:44 +0000

    Updated web.js with index.html again10

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
+
+
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
